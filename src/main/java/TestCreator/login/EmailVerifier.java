563436c1c39fe3834b7c8222010c1976ffce859b
BSISJ7 <25395733+BSISJ7@users.noreply.github.com>
2023-12-12 22:06:24 -0500

    -Changed how the answer index buttons are selected during a test and their color changes when tests are graded. -cleanup methods and other changes to deal with memory issues. -Fixed MultipleCheckBoxTestPanel answers being incorrectly graded. -The text color on the question buttons during testing for partially correct questions now yellow instead of red. -Max points for MultipleCheckBox changed to the total checkbox questions. -Added showAndWait method to StackPanelAlert so that actions could be delayed until close is pressed. -addUser checks if user already exists and if email is empty. -Fixed StackPaneAlert from not showing up in addUser -Added class to css files for answers that are partially correct -UserManager changed from being accessed as a singleton to -Re-enabled email verifications being sent. -Fixed emails not be verified properly due to case. -Flagged question border colors changed

--- src/main/java/TestCreator/login/EmailVerifier.java
+++ src/main/java/TestCreator/login/EmailVerifier.java
@@ -11,33 +11,20 @@ public class EmailVerifier {
 
     public static Region EMAIL_REGION = Region.US_EAST_2;
 
-    public static void verifyEmail(String email) {
-        System.setProperty("aws.accessKeyId", System.getenv("AWS_SES_ACCESS_KEY"));
-        System.setProperty("aws.secretAccessKey", System.getenv("AWS_SES_SECRET_ACCESS_KEY"));
-
-        SesClient client = SesClient.builder()
-                .region(EMAIL_REGION)
-                .build();
+    public static void verifyEmail(String email) throws RuntimeException{
+        SesClient client = SesClient.builder().region(EMAIL_REGION).build();
 
         try {
             VerifyEmailIdentityRequest request = VerifyEmailIdentityRequest.builder()
-                    .emailAddress(email)
-                    .build();
+                    .emailAddress(email.toLowerCase()).build();
             client.verifyEmailIdentity(request);
-            System.out.println("A verification email has been sent to " + email);
-
-        } catch (
-                SesException e) {
-            System.out.println(e.getMessage());
-            System.exit(1);
+        } catch (SesException e) {
+            throw new RuntimeException(e);
         }
         client.close();
     }
 
     public static boolean isEmailVerified(String email){
-        System.setProperty("aws.accessKeyId", System.getenv("AWS_SES_ACCESS_KEY"));
-        System.setProperty("aws.secretAccessKey", System.getenv("AWS_SES_SECRET_ACCESS_KEY"));
-
         SesClient client = SesClient.builder()
                 .region(EMAIL_REGION)
                 .build();
