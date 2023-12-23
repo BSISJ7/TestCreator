540a9c00ad431e55034e9ec0a851ab89506fab63
laxle <25395733+BSISJ7@users.noreply.github.com>
2023-12-22 22:26:35 -0500

    -Changed how FillTheBlankEditor handles pasted text. -Added methods to CaretUtilities to make use of regex when checking where to start and stop searching. -When leaving the TextArea of FillTheBlankEditor or going outside the text length the while adding/removing an answer the style color is now removed properly and added if the same previous word is hovered by the mouse. -When removing a word only answers have their color changed now.

--- src/main/java/TestCreator/questions/FillTheBlank.java
+++ src/main/java/TestCreator/questions/FillTheBlank.java
@@ -11,11 +11,6 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import javax.swing.text.DefaultHighlighter;
-import javax.swing.text.Highlighter.HighlightPainter;
-import javax.swing.text.SimpleAttributeSet;
-import javax.swing.text.StyleConstants;
-import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,45 +19,19 @@ import java.util.Objects;
 import static TestCreator.testIO.XMLIO.findNode;
 
 public class FillTheBlank extends Question {
-
-    public final static SimpleAttributeSet UNSELECTED_WORD = new SimpleAttributeSet();
-    public final static SimpleAttributeSet SELECTED_WORD = new SimpleAttributeSet();
-    public final static SimpleAttributeSet INCORRECT_WORD = new SimpleAttributeSet();
-    public final static SimpleAttributeSet CORRECT_WORD = new SimpleAttributeSet();
-
-    public final static HighlightPainter NEW_WORD_PAINT = new DefaultHighlighter.DefaultHighlightPainter(new Color(196, 238, 129));
-    public final static HighlightPainter SELECTED_WORD_PAINT = new DefaultHighlighter.DefaultHighlightPainter(new Color(193, 239, 248));
-    public final static HighlightPainter DELETE_WORD_PAINT = new DefaultHighlighter.DefaultHighlightPainter(new Color(248, 96, 97));
-    public static final String UNDERLINE_WORD = "fx-underline: true;";
-
     public static final String ANSWER = ("-fx-fill: blue;");
-    public static final String REMOVAL = ("-fx-fill: red;");
-    public static final String HOVER = ("-fx-fill: dodgerblue;");
     public static final String DEFAULT = ("-fx-fill: black;");
     public static final String CORRECT = ("-fx-fill: green;");
     public static final String INCORRECT = ("-fx-fill: red;");
-
-    static {
-        StyleConstants.setUnderline(UNSELECTED_WORD, true);
-
-        StyleConstants.setUnderline(SELECTED_WORD, true);
-        StyleConstants.setBackground(SELECTED_WORD, new Color(193, 239, 248));
-
-        StyleConstants.setUnderline(INCORRECT_WORD, true);
-        StyleConstants.setBackground(INCORRECT_WORD, new Color(248, 96, 97));
-
-        StyleConstants.setUnderline(CORRECT_WORD, true);
-        StyleConstants.setBackground(CORRECT_WORD, Color.GREEN.brighter().brighter());
-    }
 
     public static final String ALLOWED_CHARS = "[a-zA-Z0-9-'_]";
 
 
     private final ObservableList<String> wordBank = FXCollections.observableArrayList();
     private String fillQuestion = "";
-    private ArrayList<Integer> wordPositions = new ArrayList<>();
+    private final ArrayList<Integer> wordPositions = new ArrayList<>();
 
-    private SelectionManager selectionManager = new SelectionManager();
+    private final SelectionManager selectionManager = new SelectionManager();
 
     private boolean displayAnswers = true;
 
